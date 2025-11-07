b8b78966e3fcb05cb988b0ddd2999612645dbc90
Chaitanya-963 <chaitanyamehetre0301@gmail.com>
2025-11-07 10:04:13 +0530

    New Start

--- 02_Strings/String01.java
+++
@@ -1,9 +0,0 @@
-public class String01 {
-    public static void main(String[] args) {
-        String morning = "Good morning";
-
-        String night = new String("Good night");
-
-         System.out.println(morning + " " + night);
-    }
-}
