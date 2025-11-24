725cd4b88887ee661858d45450d14aceaa6be982
Chaitanya-963 <chaitanyamehetre0301@gmail.com>
2025-11-24 17:28:06 +0530

    Learning Java Language

---
+++ JavaProgramming/src/ArithmeticClass.java
@@ -0,0 +1,46 @@
+public class ArithmeticClass {
+    public static void main(String[] args) {
+
+        // Arithmetic Operators
+//        int x = 10;
+//        int y = 3;
+//        int z;
+
+//        z = x + y;
+//        z = x - y;
+//        z = x * y;
+//        z = x / y;
+//        z = x % y;
+//        System.out.println(z);
+
+        // Augmented Assignment Operators
+
+//        int x = 10;
+//        int y = 5;
+//
+//        x += y;
+//        x -= y;
+//        x *= y;
+//        x /= y;
+//        x %= y;
+//
+//        System.out.println(x);
+
+
+        // Increment and Decrement Operators
+
+//        int x = 1;
+//        int y = 3;
+
+//        x++;
+//        x--;
+
+//        System.out.println(x);
+
+        // ORDER OF OPERATORS [P-E-M-D-A-S]
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+
+        System.out.println(result);
+    }
+}
