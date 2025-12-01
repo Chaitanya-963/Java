81945fd387d48953dbb4bf9467037e697af47357
Chaitanya-963 <chaitanyamehetre0301@gmail.com>
2025-12-01 10:14:38 +0530

    Learn about Math class, Printf and Random class

--- JavaProgramming/src/MathClass.java
+++ JavaProgramming/src/MathClass.java
@@ -1,4 +1,63 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class MathClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//        System.out.println(Math.PI);
+
+//        double result;
+//        result = Math.pow(4, 5);
+//        result = Math.abs(-48);
+//        result = Math.sqrt(25);
+//        result = Math.round(25.5114);
+//        result = Math.ceil(25.5114);
+//        result = Math.floor(25.5114);
+//        result = Math.max(25, 45);
+//        result = Math.min(25, 45);
+//        System.out.println(result);
+
+
+        //Hypotenuse
+
+
+//        double a;
+//        double b;
+//        double c;
+//
+//        System.out.print("Enter the length of side A: ");
+//        a = scanner.nextDouble();
+//
+//        System.out.print("Enter the length of side B: ");
+//        b = scanner.nextDouble();
+//
+//        c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+//
+//        System.out.println("The Hypotenuse is " + c + "cm");
+
+
+        // Circumference = 2 * Math.PI * radius;
+        //area = Math.PI * Math.pow(radius,2)
+        //Volume = (4.0/2.0) * Math.PI * Math.pow(radius,3)
+
+        double radius;
+        double circumference;
+        double area;
+        double volume;
+
+        System.out.print("Enter your radius: ");
+        radius = scanner.nextDouble();
+
+        circumference = 2 * Math.PI * radius;
+        area = Math.PI * Math.pow(radius, 2);
+        volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+
+
+        System.out.printf("The circumference is: %.1fcm ", circumference);
+        System.out.printf("\nThe area is %.1fcm²", area);
+        System.out.printf("\nThe area is %.1fcm³", volume);
+
+
+        scanner.close();
+    }
 }
--- JavaProgramming/src/PrintfClass.java
+++ JavaProgramming/src/PrintfClass.java
@@ -1,4 +1,65 @@
-package PACKAGE_NAME;
-
 public class PrintfClass {
+    public static void main(String[] args) {
+
+        // printf() = is a method used to format output
+        // %[flags][width][.precision][specifier-character]
+
+        // [specifier - character]
+//        String name = "Chaitanya";
+//        char firstLetter = 'C';
+//        int age = 22;
+//        double height = 5.6;
+//        boolean isEmployed = true;
+//
+//        System.out.printf("Hello %s\n", name);
+//        System.out.printf("Your name start with a %c\n", firstLetter);
+//        System.out.printf("Your age is a %d\n", age);
+//        System.out.printf("Your height is a %.1f\n", height);
+//        System.out.printf("Your are Employed(True/False) %b\n", isEmployed);
+//
+//        System.out.printf("%s is %d years old", name, age);
+
+        // [.precision]
+//        double price1 = 9.99;
+//        double price2 = 100.155;
+//        double price3 = -54.45;
+//
+//        System.out.printf("%.1f\n", price1);
+//        System.out.printf("%.1f\n", price2);
+//        System.out.printf("%.1f\n", price3);
+
+        // [flags]
+
+//        + --> output a plus
+//        , --> comma grouping are enclose in ()
+//        ( --> negative numbers are enclosed in ()
+//        space = display a minus if negative, space if positive
+
+//        double price1 = 9.99;
+//        double price2 = 100.155;
+//        double price3 = -54.45;
+//
+//        System.out.printf("%+.1f\n", price1);
+//        System.out.printf("%,.1f\n", price2);
+//        System.out.printf("%(.1f\n", price3);
+//        System.out.printf("% .1f\n", price3);
+
+        //[width]
+
+//        0 = zero padding
+//        number = right justified padding
+//        negative number = left justified padding
+
+        int id1 = 1;
+        int id2 = 24;
+        int id3 = 524;
+        int id4 = 7524;
+
+        System.out.printf("%04d\n", id1);
+        System.out.printf("%4d\n", id2);
+        System.out.printf("%-4d\n", id3);
+        System.out.printf("%04d\n", id4);
+        System.out.printf("%4d\n", id4);
+        System.out.printf("%-4d\n", id4);
+    }
 }
